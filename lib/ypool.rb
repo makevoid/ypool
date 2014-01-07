d9749a6390879236f785bae4f1bd6eb3c7c82bb9
Francesco Canessa <makevoid@gmail.com>
2014-01-07 03:51:16 +0100

    unleash ruby 2.0 net http api

--- lib/ypool.rb
+++ lib/ypool.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 require 'ypool/version'
 
 class Ypool
@@ -5,7 +7,7 @@ class Ypool
   HOST = ""
 
   URLS = {
-    global_stats: "/...",
+    global_stats: "/global_stats",
   }
 
   class Connection
@@ -15,6 +17,15 @@ class Ypool
       @api_key = api_key
       # todo: integrate net_http with keepalive
     end   
+   
+    def get(url, params={})
+      uri = URI URLS[url]
+      uri.query = URI.encode_www_form params
+      resp = Net::HTTP.get url
+res = Net::HTTP.get_response(uri)
+puts res.body if res.is_a?(Net::HTTPSuccess)
+      resp.
+    end 
   end
 
   def initialize(api_key=nil)
